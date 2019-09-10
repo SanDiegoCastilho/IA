205779aa0deb53a773ca635a5926762f87044019
SanDiegoCastilho <sanidegocastilho@gmail.com>
2019-09-10 10:56:47 -0300

    Vetor de dimenoes

--- Quebra Cabeca/QuebraCabeca.cpp
+++ Quebra Cabeca/QuebraCabeca.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 //Função responsável por alocar o espaço correspondente para a matriz.
-int** InstanciaMatrizObjetivo(int** Matriz, int Dimensao);
+int** InstanciaMatriz(int** Matriz, int Dimensao);
 
 //Função responsável por inicializar com os respectivos valores de cada posição da matriz.
 int** InicializaMatrixObjetivo(int** Matriz, int Dimensao);
@@ -14,19 +14,34 @@ void MostraMatriz(int** Matriz, int Dimensao);
 int main(){
 	int Dimensao;
     int** MatrizObjetivo;
+    int** MatrizInicial;
+    int*  Distancias;
 
 	cout << "Digite a dimensão da Matrix: ";
 	cin >> Dimensao;
 
-	MatrizObjetivo = InstanciaMatrizObjetivo(MatrizObjetivo, Dimensao);
+	MatrizObjetivo = InstanciaMatriz(MatrizObjetivo, Dimensao);
 	MatrizObjetivo = InicializaMatrixObjetivo(MatrizObjetivo, Dimensao);
 	MostraMatriz(MatrizObjetivo, Dimensao);
+
+	MatrizInicial = InstanciaMatriz(MatrizInicial, Dimensao);
 	
+	cout << "Entre com a Matriz Inicial" << endl;
+	for (int i = 0; i < Dimensao; i++){
+		for (int j = 0; j < Dimensao; j++){
+			cout << "posição (" << i << ", " << j << "): ";
+			cin >> MatrizInicial[i][j];
+			cout << endl;
+		}
+	}
+
+	MostraMatriz(MatrizInicial, Dimensao);
+
 	return 0;
 }
 
 //Função responsável por alocar o espaço correspondente para a matriz.
-int** InstanciaMatrizObjetivo(int** Matriz, int Dimensao){
+int** InstanciaMatriz(int** Matriz, int Dimensao){
 	Matriz = new int *[Dimensao];
 
 	for (int i = 0; i < Dimensao; i++){
