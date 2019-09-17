0ece2d72081eb74301b6c6896855dc4956e2b7c7
San Diego Castilho <sandiegoocastilho@gmail.com>
2019-09-16 22:07:10 -0300

    Gerando filhos - incompleto

--- Quebra Cabeca/No.cpp
+++ Quebra Cabeca/No.cpp
@@ -8,15 +8,42 @@ class No{
         int Altura;
         bool EhSolucao;
 
-    No(int** Matriz, No* Pai, int** MatrizObjetivo, int Dimensao){
+    No(int** Matriz, int** MatrizObjetivo, int Dimensao){
         this->Matriz = Matriz;
-        this->Pai = Pai;
+        //this->Pai = Pai;
         this->Altura = 0;
+        this->Filhos = new No* [4];
         VerificarSolucao(MatrizObjetivo, Dimensao);
     }
 
-    void GerarFilhos(){
+    void GerarFilhos(int Dimensao){
+        int u, v;
 
+        for (int i = 0; i < Dimensao; i++){
+            for (int j = 0; j < Dimensao; j++){
+                if (this->Matriz[i][j] == 0){
+                    u = i;
+                    v = j;
+                }
+            }
+        }
+
+        int** MatrizAux1 = this-> Matriz;
+        Mover(u+1, v, MatrizAux1, Dimensao);
+        //this->Filhos[0] = new No(MatrizAux1, );
+
+        int** MatrizAux2 = this-> Matriz; 
+        Mover(u, v+1, MatrizAux2, Dimensao);
+        //this->Filhos[1] = new No();
+
+        int** MatrizAux3 = this-> Matriz; 
+        Mover(u-1, v, MatrizAux3, Dimensao);
+        //this->Filhos[2] = new No();
+
+        int** MatrizAux4 = this-> Matriz; 
+        Mover(u, v-1, MatrizAux4, Dimensao);
+        //this->Filhos[3] = new No();
+    
     }
 
     void VerificarSolucao(int** MatrizObjetivo, int Dimensao){
